82c185e9489d9fc5eef47f179c1ef66b44776bd9
miche <michelangelofantini.mail@gmail.com>
2025-03-17 11:26:58 -0400

    vicon node

---
+++ flight_club/src/exercise2/vicon_node.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+from rclpy.qos import QoSProfile, ReliabilityPolicy
+
+
+class CameraPoseForward(Node):
+    def __init__(self):
+        super().__init__('camera_pose_forward')
+        # qos_profile = QoSProfile(depth=10)
+        # qos_profile.reliability = ReliabilityPolicy.BEST_EFFORT
+
+        self.subscription = self.create_subscription(
+            PoseStamped,
+            '/vicon/ROB498_Drone/ROB498_Drone',
+            self.pose_callback,
+        )
+        self.publisher = self.create_publisher(
+            PoseStamped,
+            '/mavros/vision_pose/pose',
+            10
+        )
+        self.get_logger().info("camera_pose_forward node started")
+
+    def pose_callback(self, msg):
+        # Create a new PoseStamped message and transfer the pose data from Odometry.
+        new_message = PoseStamped()
+        # Copy header information (timestamp, frame id)
+        new_message.header.stamp = msg.header.stamp
+        new_message.header.frame_id = msg.header.frame_id
+        # Extract the pose from the Odometry message
+        new_message.pose = msg.pose.pose
+        self.publisher.publish(new_message)  # Forward the converted pose message
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = CameraPoseForward()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
